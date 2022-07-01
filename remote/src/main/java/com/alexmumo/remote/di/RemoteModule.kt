2c0d44b4e84db60b391a6bd76ddd2895f2ae78e9
alexymumo <alexymumo2000@gmail.com>
2022-07-01 15:20:19 +0300

    common module

--- remote/src/main/java/com/alexmumo/remote/di/RemoteModule.kt
+++ remote/src/main/java/com/alexmumo/remote/di/RemoteModule.kt
@@ -1,9 +1,32 @@
 package com.alexmumo.remote.di
 
+import com.alexmumo.remote.api.MovieApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val remoteModule = module {
     single {
-
+        Retrofit.Builder()
+            .baseUrl("dekut.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttp())
+            .build()
+            .create(MovieApi::class.java)
     }
+}
+
+fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+    return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC)
+}
+
+fun provideOkHttp(): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor())
+        .readTimeout(15, TimeUnit.SECONDS)
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .build()
 }
